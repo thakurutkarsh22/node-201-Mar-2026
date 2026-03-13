931860876cb24d3d25297aa2f97283aecdef50ff
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-03-13 20:58:46 +0530

    session 6 code

--- Session6/Routes/BlogsRoute.js
+++ Session6/Routes/BlogsRoute.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
 const { getAllBlogs, deleteBlogById, createBlog, getBlogById } = require('../Controllers/BlogsController');
+const BlogInputValidationMiddleware = require('../Middleware/BlogInputValidationMiddleware');
 const router = express.Router();
 
 
 router.get("/getAllBlogs", getAllBlogs);
 router.get("/getBlogById/:id", getBlogById);
 
-router.post("/createBlog", createBlog);
+router.post("/createBlog", BlogInputValidationMiddleware, createBlog);
 
 
 router.delete("/deleteBlogById/:id", deleteBlogById);
