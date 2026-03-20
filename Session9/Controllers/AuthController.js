a3391fff405c0c542109f683c99ca9755533f1fc
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-03-20 20:29:15 +0530

    session 9 precode

---
+++ Session9/Controllers/AuthController.js
@@ -0,0 +1,55 @@
+const UserModel = require("../Models/UserModel");
+const AuthService = require("../Services/AuthService");
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns {Promise<void>}
+ */
+
+async function register(req, res) {
+
+    const body = req.body; 
+    const { name, email, password, age, contact, gender } = body;
+
+
+    try {
+        const response = await AuthService.register(name, email, password, age, contact, gender);
+        res.status(201).json({
+            message: "user registered successfully",
+            data: response
+        })
+    } catch(error){
+        res.status(500).json({
+            message: "error occurred while registering user",
+            error: error
+        })
+    }
+
+
+}
+
+
+async function login(req, res) {
+    const body = req.body;
+    const { email, password } = body;
+
+    try {
+        const response = await AuthService.login(email, password);
+        res.status(200).json({
+            message: "user logged in successfully",
+            data: response
+        })
+    } catch(error){
+        res.status(500).json({
+            message: "error occurred while logging in user",
+            error: error.message
+        })
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
---
+++ Session9/Services/AuthService.js
@@ -0,0 +1,71 @@
+const UserModel = require("../Models/UserModel");
+const bcrypt = require("bcrypt");
+
+// Dependency injection 
+class AuthService {
+
+    static async register (name, email, password, age, contact, gender) {
+        // 1.  create an userModel object of this info - LOGIC
+
+        const hashedPassword = await AuthService.encryptPassword(password);
+
+        const userObject = UserModel({
+            name, email, password: hashedPassword, age, contact, gender
+        });
+
+
+        // 2. talk to database and save this object in database
+
+        try {
+            const response = await userObject.save();
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    static async encryptPassword (password) {
+        const salt = await bcrypt.genSalt(10); // how many times we want to hash it 
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    }
+
+    static async login (email, password) {
+        // find out if the email is valid or not, user is there with that email or not 
+
+        try {
+            const user = await AuthService.findUserByEmail(email);
+
+            if(!user) {
+                throw new Error("user not found with this email");
+            } else {
+                const passwordFromDatabase = user.password; // hashed password $2b$10$1BwURKl99ufj0z96GwTunehlwAaKGvpAav0dq/SGZHnAvGiIet6I.
+
+                const isPasswordMatching = await bcrypt.compare(password, passwordFromDatabase); // true or false
+
+                if(isPasswordMatching) {
+                    return user;
+                } else {
+                    throw new Error("invalid password");
+                }
+
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
+
+    }
+
+    static async findUserByEmail (email) {
+        try {
+            const user = await UserModel.findOne({ email: email})
+            return user;
+        } catch (error) {
+            return null;
+        }
+    }
+
+}
+
+
+module.exports = AuthService;
